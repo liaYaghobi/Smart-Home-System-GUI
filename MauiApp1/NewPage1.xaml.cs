02e28089900125070132a6363ba9d149bd6248b0
liaYaghobi <107156726+liaYaghobi@users.noreply.github.com>
2022-11-26 00:28:20 -0800

    working on making a parent "display device" class

--- MauiApp1/NewPage1.xaml.cs
+++ MauiApp1/NewPage1.xaml.cs
@@ -1,44 +1,59 @@
 
-using System.ComponentModel;
-using System.Net.Mime;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml.Linq;
 using MauiApp1.Helpers;
 using Syncfusion.Maui.Sliders;
-//using Microsoft.UI.Xaml.Controls;
 using Button = Microsoft.Maui.Controls.Button;
-using Slider = Microsoft.Maui.Controls.Slider;
+
 
 namespace MauiApp1;
 
 public partial class NewPage1 : ContentPage
 {
- 
+
+    int bulbIndex = 1;
 
     public NewPage1()
 	{
 		InitializeComponent();
 	}
 
-    public void LivingRoomLight(object sender, EventArgs e)
+    public void displayLabel(Label r)      /* TO DISPLAY THE ROOM LABELS*/
     {
-
-        if (!Label1.IsVisible)
+        if (!r.IsVisible)
         {
-            Label1.IsVisible = true;
+            r.IsVisible = true;
         }
+    }
 
-        var btn = new Button
-        {   HeightRequest = 80,
-            WidthRequest = 100,
-            CornerRadius = 20,
+    public Label createLightBulb()      /*CREATES AND RETURNS LIGHTBULB ICON*/
+    {
+        var bulb = new Label
+        {
             FontSize = 30,
             FontFamily = "MaterialDesignIcons",
             Text = MaterialDesignIconFonts.LightbulbOnOutline,
-            BackgroundColor = Colors.CadetBlue,
             HorizontalOptions = LayoutOptions.Center
         };
+
+        return bulb;
+
+    }
+
+    public Label displayDeviceName()
+    {
+        
+        var l = new Label
+        {
+            Text = $"Light Bulb {bulbIndex}",
+            HorizontalOptions = LayoutOptions.Center,
+
+        };
+
+        bulbIndex++;
+        return l;
+    }
+
+    public Button delete()
+    {
         var del = new Button
         {
             HeightRequest = 40,
@@ -50,8 +65,10 @@ public partial class NewPage1 : ContentPage
             BackgroundColor = Colors.IndianRed,
             HorizontalOptions = LayoutOptions.Start
         };
-
-
+        return del;
+    }
+    public SfSlider slide()
+    {
         SfSlider s = new SfSlider()
         {
             Minimum = 0,
@@ -59,83 +76,74 @@ public partial class NewPage1 : ContentPage
             Value = 0,
         };
 
+        return s;
+    }
 
-        del.Clicked += OnClick;
+    public void toggleLight(double value, Label bulb)
+    {
+        if (value == 0)
+        {
+            bulb.Text = MaterialDesignIconFonts.LightbulbOnOutline;
+        }
+        if (value > 0 && value < 40)
+        {
+            bulb.Text = MaterialDesignIconFonts.LightbulbOn40;
+        }
+        if (value > 40 && value < 60)
+        {
+            bulb.Text = MaterialDesignIconFonts.LightbulbOn60;
+        }
+        if (value > 60 && value < 80)
+        {
+            bulb.Text = MaterialDesignIconFonts.LightbulbOn80;
+        }
+        if (value == 100)
+        {
+            bulb.Text = MaterialDesignIconFonts.LightbulbOn;
+        }
+
+    }
+    public void LivingRoomLight(object sender, EventArgs e)
+    {
+
+        displayLabel(Label1);            /*  room title */
+        var l = displayDeviceName();     /*  device title */
+        var bulb = createLightBulb();   /*  lightbulb   */
+        var del = delete();             /*  delete   */
+        var s = slide();                /*  toggle   */
+
+        del.Clicked += OnClick;               /* register event handlers*/
         s.ValueChanged += OnValueChanged;
 
-
-        V.Add(del);
-        V.Add(btn);
+        V.Add(del);                        /* push onto display stack   */
+        V.Add(l);
+        V.Add(bulb);
         V.Add(s);
-
        
+
         void OnValueChanged(object sender, SliderValueChangedEventArgs e)
         {
             double value = e.NewValue;
-
-            if (value == 100)
-            {
-
-                btn.Text = MaterialDesignIconFonts.LightbulbOn40;
-
-            }
+            toggleLight(value,bulb);
         }
 
         void OnClick(object sender, EventArgs e)
         {
-            V.Remove(btn);
-            btn = null;
-            V.Remove(s);
-            s = null;
-            V.Remove(del);
-            del = null;
-        }
-
-    }
-
-
-
-    private void LivingRoomSpeaker(object sender, EventArgs e)
-    {
-        
-
-        if (!Label1.IsVisible)
-        {
-            Label1.IsVisible = true;
-        }
-        
-        var btn = new Button
-        {
-            HeightRequest = 80,
-            WidthRequest = 100,
-            CornerRadius = 20,
-            FontSize = 30,
-            FontFamily = "MaterialDesignIcons",
-            Text = MaterialDesignIconFonts.VolumeLow,
-            BackgroundColor = Colors.CadetBlue,
-            HorizontalOptions = LayoutOptions.Center
-        };
-
-
-
-        Slider s = new Slider
-        {
-            Minimum = 0,
-            Maximum = 100,
-           
-
-
-        };
-
-       V.Add(btn);
-       V.Add(s);
-
-
- 
-       
+            int i = V.IndexOf(del);
      
+            V.RemoveAt(i+3);
+            V.RemoveAt(i+2);
+            V.RemoveAt(i+1);
+            V.RemoveAt(i);
+
+
+        }
+
     }
 
     
 }
+
+
+
 
